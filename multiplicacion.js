e42656ec29a6d772957bd40b74507edfdd2e78c6
emilianostic <emilianostic@gmail.com>
2023-05-20 17:46:21 -0300

    primer commit

---
+++ multiplicacion.js
@@ -0,0 +1,8 @@
+a = 2;
+b = 17;
+c = 6;
+
+multiplicacion = a * b * c; //204 resultado
+console.log('El producto es: ' + multiplicacion)
+
+
---
+++ resta.js
@@ -0,0 +1,9 @@
+//parte a
+x = 10; //x es el minuendo
+y = 2; //y es el sustraendo
+
+resta = x - y; //resultado 8
+
+console.log('El resultado de la resta es: ' + resta);
+
+
