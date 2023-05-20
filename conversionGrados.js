d13d5de8868d79abcbdbfd973d63eb7557ca26e6
emilianostic <emilianostic@gmail.com>
2023-05-20 18:04:34 -0300

    agrego el archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,13 @@
+//conversión de grados Fahrenheit (F) a grados Celsius (C)
+
+//c =  (f - 32) * 5/9
+
+f = 50;
+
+c = (50 - 32) * 5/9;
+console.log('50 grados Fahrenheit equivalen a ' + c +' '+ 'grados Celsius');
+
+f = 75;
+
+c = (75 - 32) * 5/9;
+console.log('75 grados Fahrenheit equivalen a ' + c +' '+ 'grados Celsius');
