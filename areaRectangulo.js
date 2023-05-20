5707388db2928e0a50964714f0974e20430e426f
emilianostic <emilianostic@gmail.com>
2023-05-20 18:06:34 -0300

    Practico N0 Finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,13 @@
+// a = b * h es area de un rectángulo  = base * altura
+b = 5;
+h = 3;
+
+a = b * h; // si b = 5 y h = 3, el área es 15 cm
+//si 
+console.log('El área es: ' + a);
+
+b = 12.5
+h = 5.3
+
+a = b * h;
+console.log('El área es: ' + a)
---
+++ promedio3.js
@@ -0,0 +1,24 @@
+//promedio de 3 números
+x = 5;
+y = 7;
+z = 9;
+promedio = (x + y + z)/3;
+
+
+console.log('El promedio entre 5, 7 y 9 es: ' + promedio)
+
+x = -2;
+y = -3;
+z = -8;
+promedio = (x + y + z)/3;
+
+
+console.log('El promedio entre -2, -3 y -8 es: ' + promedio)
+
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x + y + z)/3;
+
+
+console.log('El promedio entre -1.2, 5.3 y -3.3 es: ' + promedio)
