a3d0161acd87b18683563f445d4413c24afa2f1d
emilianostic <emilianostic@gmail.com>
2023-06-17 13:27:23 -0300

    agrego modificado positivoNegativo.js

--- practico2/positivoNegativo.js
+++ practico2/positivoNegativo.js
@@ -1,14 +1,4 @@
-const readlineSync = require('readline-sync');
-const numero = readlineSync.question('Por favor, ingresa un numero: ');
 
-if(numero > 0){
-    console.log("El numero es positivo");
-}
-else if(numero == 0){
-console.log("El numero es cero");
-}
-else{
-    console.log("El numero es negativo");
     const readlineSync = require('readline-sync');
     const numero = readlineSync.question('Por favor, ingresa un numero: ');
     
